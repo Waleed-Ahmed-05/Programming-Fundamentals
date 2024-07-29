2fc553fe898b5bdc707f32aa2bcdd3adc4599afb
Waleed-Ahmed-05 <LP.Gangster05@gmail.com>
2024-07-29 20:00:43 +0500

    Uploaded via Git Bash

---
+++ Week_05/Lab/Sleep.cpp
@@ -0,0 +1,14 @@
+// Write a program that halts the execution for 200 milliseconds using the Sleep function.
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        cout << "LP-Gangster";
+        Sleep(200);
+    }
+}
