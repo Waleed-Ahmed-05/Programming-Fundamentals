7634d657d6b5e37fb0cfc9193b11d72ea49bbcc6
Waleed-Ahmed-05 <LP.Gangster05@gmail.com>
2024-07-31 19:56:05 +0500

    Uploaded via Git Bash

---
+++ Week_05/PD/Prediction.cpp
@@ -0,0 +1,16 @@
+// Write the below-mentioned program to verify whether your prediction is true or false.
+
+#include <iostream>
+using namespace std;
+
+int x = 10;
+
+void Function()
+{
+    int x = 20;
+}
+int main()
+{
+    Function();
+    cout << "The value of x is " << x << ".";
+}
